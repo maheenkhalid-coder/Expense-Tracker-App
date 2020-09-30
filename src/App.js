aeb17a855c48c5150982539a497cbec5b2048cf6
maheenkhalid-coder <64140831+maheenkhalid-coder@users.noreply.github.com>
2020-10-01 01:23:50 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Header } from './Components/Header';
+import { Balance } from './Components/Balance';
+import { IncomeExpenses } from './Components/IncomeExpenses';
+import { TransactionList } from './Components/TransactionList';
+import { AddTransaction } from './Components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div class="container">
+        <Balance /> 
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+      
+
+    </GlobalProvider>
+  );
+}
+
+export default App;
